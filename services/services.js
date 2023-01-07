a82a9056395a22c081f90e47630ca90550f0c024
IMRANDIL <aliimranadil2@gmail.com>
2023-01-07 08:42:15 +0530

    second react_native app movies app

---
+++ services/services.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const apiUrl = '';
+const apiKey = '';
+
+const getPopularMovies = async () => {
+  try {
+    const response = await axios.get(``);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUpcomingMovies = async () => {
+  try {
+    const response = await axios.get(``);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getPopularTV = async () => {
+  try {
+    const response = await axios.get(``);
+  } catch (error) {
+    console.log(error);
+  }
+};
