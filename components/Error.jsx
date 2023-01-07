dd211fb7f6bf467a8cb36558b85543f86f7f671d
IMRANDIL <aliimranadil2@gmail.com>
2023-01-07 19:09:50 +0530

    some more

---
+++ components/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+class Error extends React.PureComponent {
+  render() {
+    const {errorText1, errorText2} = this.props;
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+export default Error;
