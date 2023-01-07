dbed05d831398e4976d96d7b64c6d46aa2a8c26d
IMRANDIL <aliimranadil2@gmail.com>
2023-01-07 19:13:33 +0530

    some more

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 class Error extends React.PureComponent {
   render() {
     const {errorText1, errorText2} = this.props;
     return (
-      <View>
-        <Text></Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>{errorText1}</Text>
+        <Text style={styles.text}>{errorText2}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+  },
+});
 
 export default Error;
