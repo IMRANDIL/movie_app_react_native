187ae1ce5020fea86e5bb61adcd185038ec41722
IMRANDIL <aliimranadil2@gmail.com>
2023-01-14 19:05:11 +0530

    some more push

--- components/PlayButton.js
+++ components/PlayButton.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
-const PlayButton = () => {
-  return <div>PlayButton</div>;
-};
+import {Pressable, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+class PlayButton extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <Pressable>
+          <Icon name={'caret-forward-outline'}></Icon>
+        </Pressable>
+      </>
+    );
+  }
+}
 
 export default PlayButton;
