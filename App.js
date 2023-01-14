0e2b756926cc3533f3e21c91e3f9e7b78e1c4afd
IMRANDIL <aliimranadil2@gmail.com>
2023-01-14 11:54:02 +0530

    some more tweak

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './screens/Home';
+import Details from './screens/Details';
 
 const Stack = createStackNavigator();
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
