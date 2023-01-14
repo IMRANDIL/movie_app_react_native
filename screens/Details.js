b2069507f8377719fd8afa688ef46c74c99c4790
IMRANDIL <aliimranadil2@gmail.com>
2023-01-14 14:05:02 +0530

    some mroe

--- screens/Details.js
+++ screens/Details.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, ActivityIndicator, View} from 'react-native';
 
 const Details = ({route}) => {
   const {movieDetail} = route.params;
   return (
     <>
+      {!movieDetail && <ActivityIndicator size="large" color="#0000ff" />}
+
       <Text>{movieDetail.title}</Text>
     </>
   );
