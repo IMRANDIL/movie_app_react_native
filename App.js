7339f4a2191fbb87b3398a9c59c8ac0035e5e254
IMRANDIL <aliimranadil2@gmail.com>
2023-01-14 11:37:46 +0530

    saturday push

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './screens/Home';
 
+const Stack = createStackNavigator();
 const App = () => {
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-      }}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
